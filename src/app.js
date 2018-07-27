917380989a8345e99c77c3df44bb6c11cf4de33c
karine <kitana.91@mail.ru>
2018-07-27 14:46:40 +0300

    get firebase data

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
+import { startSetExpenses } from './actions/expenses';
 import getVisibleExpenses from './selectors/expenses';
 import 'react-dates/initialize';
 import 'normalize.css';
@@ -19,5 +19,9 @@ const jsx = (
         <AppRouter />
     </Provider>
 );
+ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
 
-ReactDOM.render(jsx, document.getElementById('app'));
+store.dispatch(startSetExpenses()).then(() => {
+    ReactDOM.render(jsx, document.getElementById('app'));
+});
+
